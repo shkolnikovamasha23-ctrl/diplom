dee9328bf195cb53d89a1b4b1586bd42b8d9f926
kraken <shkolnikova.masha23@gmail.com>
2026-06-05 21:24:34 +0300

    Добавление точки входа приложения

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Точка входа для запуска веб-сайта EduSim.
+
+Файл находится в корневом каталоге, как требуется в индивидуальном задании.
+"""
+from edusim import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
